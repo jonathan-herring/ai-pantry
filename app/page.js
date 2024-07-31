5ce9e453cea30ff7177cf74826f28f3ce16fdc5d
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-07-30 17:16:25 -0700

    refactor: Update page.js to include additional pantry items and improve styling

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
 import { Box, Stack, Typography } from "@mui/material";
 
-const item = ["tomato", "potato", "onion", "garlic", "ginger", "carrot"];
+const item = [
+  "tomato",
+  "potato",
+  "onion",
+  "garlic",
+  "ginger",
+  "carrot",
+  "lettuce",
+  "kale",
+  "cucumber",
+];
 
 export default function Home() {
   return (
@@ -12,35 +22,37 @@ export default function Home() {
       flexDirection={"column"}
       alignItems={"center"}
     >
-      <Box
-        width="800px"
-        height="100px"
-        bgcolor={"#ADD8E6"}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
-          Pantry Items
-        </Typography>
+      <Box border={"1px solid #333"}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={"#ADD8E6"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
+            Pantry Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
+          {item.map((i) => (
+            <Box
+              key={i}
+              width="100%"
+              height="100px"
+              bgcolor={"#f0f0f0"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Typography variant={"h3"} color={"#333"} textAligh={"center"}>
+                {i.charAt(0).toUpperCase() + i.slice(1)}
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
       </Box>
-      <Stack width="880px" height="200px" spacing={2} overflow={"auto"}>
-        {item.map((i) => (
-          <Box
-            key={i}
-            width="100%"
-            height="100px"
-            bgcolor={"#f0f0f0"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Typography variant={"h3"} color={"#333"} textAligh={"center"}>
-              {i.charAt(0).toUpperCase() + i.slice(1)}
-            </Typography>
-          </Box>
-        ))}
-      </Stack>
     </Box>
   );
 }
