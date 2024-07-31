a9280724a1e6023e5905bd645580a479558c0bf8
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-07-30 17:29:12 -0700

    chore: Update firebase.js to use environment variables for Firebase configuration

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
